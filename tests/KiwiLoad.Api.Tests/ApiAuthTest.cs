3474113eb61c5f2345e3d1a73b9cfba046b1b1bd
Rafal Tynski <rafal.tynski@gmail.com>
2024-03-10 23:51:33 +0100

    #22 Create login test

---
+++ tests/KiwiLoad.Api.Tests/ApiAuthTest.cs
@@ -0,0 +1,62 @@
+﻿using FluentAssertions;
+using KiwiLoad.Api.Controllers.Auth.Login.V1.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using Newtonsoft.Json;
+using System.Net;
+using System.Text;
+
+namespace KiwiLoad.Api.Tests;
+public class ApiAuthTest
+{
+    private readonly TestServer _server;
+    private readonly HttpClient _client;
+
+    public ApiAuthTest()
+    {
+        // Arrange
+        var testServer = new WebHostBuilder()
+            .UseEnvironment("Development")
+            .UseStartup<Startup>();
+        _server = new TestServer(testServer);
+        _client = _server.CreateClient();
+    }
+
+    [Fact]
+    public async Task GetLoginNoUsername_Should_ReturnUnauthorized()
+    {
+        // Arrange
+        var request = new HttpRequestMessage(HttpMethod.Post, "/api/auth/v1/login");
+
+        // Arrange
+        var login = new LoginReq()
+        {
+            Username = string.Empty,
+            Password = "test"
+        };
+        request.Content = new StringContent(JsonConvert.SerializeObject(login), Encoding.UTF8, "application/json");
+        var response = await _client.SendAsync(request);
+
+        // Assert
+        response.StatusCode.Should().Be(HttpStatusCode.Unauthorized);
+    }
+
+    [Fact]
+    public async Task GetLoginNoPassword_Should_ReturnUnauthorized()
+    {
+        // Arrange
+        var request = new HttpRequestMessage(HttpMethod.Post, "/api/auth/v1/login");
+
+        // Arrange
+        var login = new LoginReq()
+        {
+            Username = "Test",
+            Password = string.Empty
+        };
+        request.Content = new StringContent(JsonConvert.SerializeObject(login), Encoding.UTF8, "application/json");
+        var response = await _client.SendAsync(request);
+
+        // Assert
+        response.StatusCode.Should().Be(HttpStatusCode.Unauthorized);
+    }
+}
