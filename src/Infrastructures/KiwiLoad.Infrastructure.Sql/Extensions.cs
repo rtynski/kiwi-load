a69765cd8104b738763e4b84b205e69bda23ce12
Rafal Tynski <rafal.tynski@gmail.com>
2024-03-29 10:54:38 +0100

    #41 Fix EfCore memory

---
+++ src/Infrastructures/KiwiLoad.Infrastructure.Sql/Extensions.cs
@@ -0,0 +1,20 @@
+﻿using KiwiLoad.Core.Areas.Auth;
+using KiwiLoad.Core.Areas.Users;
+using KiwiLoad.Core.Areas.Warehouses;
+using KiwiLoad.Infrastructure.Repositories.Auth;
+using KiwiLoad.Infrastructure.Repositories.Users;
+using KiwiLoad.Infrastructure.Repositories.Warehouses;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace KiwiLoad.Infrastructure;
+
+public static class Extensions
+{
+    public static IServiceCollection AddInfrastructureBase(this IServiceCollection services)
+    {
+        services.AddTransient<IAuthRepository, AuthRepository>();
+        services.AddTransient<IUsersRepository, UsersRepository>();
+        services.AddTransient<IWarehouseRepository, WarehouseRepository>();
+        return services;
+    }
+}
